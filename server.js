c0767de37fdeb64805266b49da40840b5635f498
Hannes Geißler <h.geiler@schille.local>
2020-11-14 11:25:48 +0100

    test post method

--- server.js
+++ server.js
@@ -36,6 +36,17 @@ router.get('/getUsers', (req, res) => {
   })
 })
 
+// create method
+router.post('/postData', (req, res) => {
+  let user = new User({
+    name: 'Mila',
+  })
+  user.save((err) => {
+    if (err) return res.json({ success: false, error: err })
+    return res.json({ success: true })
+  })
+})
+
 app.use('/api', router)
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`LISTENING ON PORT ${PORT}`))
