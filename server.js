de6a3412f142b7b212e149239909c312a703534d
Hannes Geißler <h.geiler@schille.local>
2020-11-15 20:26:58 +0100

    added cors header

--- server.js
+++ server.js
@@ -92,5 +92,15 @@ router.post('/resetAll', async (req, res) => {
 })
 
 app.use('/api', router)
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  )
+  next()
+})
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`LISTENING ON PORT ${PORT}`))
